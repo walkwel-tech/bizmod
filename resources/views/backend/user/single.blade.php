84b96f4c705624ea0da07b690acf1d95c20182a1
raksha <raksha.devi@walkwel.in>
2021-04-15 09:47:58 +0530

    removed sidebar user view

--- resources/views/backend/user/single.blade.php
+++ resources/views/backend/user/single.blade.php
@@ -7,10 +7,10 @@
 
 <div class="container-fluid mt--7">
     <div class="row">
-        <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
+        <!-- <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
             <x-user.insight :user="$user"></x-user.insight>
-        </div>
-        <div class="col-xl-8 order-xl-1">
+        </div> -->
+        <div class="col-xl-12 order-xl-1">
             <div class="card bg-secondary shadow">
                 <div class="card-header bg-white border-0">
                     <div class="row align-items-center">
