e602a66cac44a1f382258544b2166b821101895c
Develope Walkwel <developer.walkwel@gmail.com>
2021-05-11 17:34:34 +0530

    client import

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -18,11 +18,13 @@ class ClientImport implements ToModel , WithUpserts, WithHeadingRow
     public function model(array $row)
     {
         return new Client([
-            'first_name'     => $row['first_name'],
-            'last_name'    => $row['last_name'],
-            'email' => $row['email'],
-            'phone' => $row['phone'],
-            'country_code' => $row['phone_code'],
+            'first_name'     => (isset($row['first_name'])) ? $row['first_name'] : '',
+            'last_name'    => (isset($row['last_name'])) ? $row['last_name'] : '',
+            'email' => (isset($row['email'])) ? $row['email'] : '',
+            'phone' => (isset($row['phone'])) ? $row['phone'] : '',
+            'country_name' => (isset($row['country_name'])) ? $row['country_name'] : '',
+            'country_code' => (isset($row['phone_code'])) ? substr($row['phone_code'],0,4) : '',
+            'zip' => (isset($row['zip'])) ? $row['zip'] : '',
         ]);
         // return new Client([
         //     'first_name'     => $row[1],
